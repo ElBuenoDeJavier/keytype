ff3802832945359ff09ec0a86a220f0188840b19
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-01 11:35:12 +0100

    cambio textos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 function App() {
   return (
-    <div className='text-center text-2xl font-bold text-blue-500 place-content-center'>
+    <div className='text-center text-2xl font-bold bg-white dark:bg-black text-blue-100 place-content-center h-screen'>
       TEXTO DE PRUEBA PARA VERIFICAR QUE TODO ESTÁ FUNCIONANDO
     </div>
   )
