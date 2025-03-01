a771c624727370cd6d86f623bd55c6f2dad27ee2
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-01 13:00:02 +0100

    Cositas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { generate } from './randomwords2/index'
+const words = generate(25).join(' ');
 function App() {
   return (
-    <div className='text-center text-2xl font-bold bg-white dark:bg-black text-blue-100 place-content-center h-screen'>
-      TEXTO DE PRUEBA PARA VERIFICAR QUE TODO ESTÁ FUNCIONANDO
-      
+    <div className='bg-black items-center h-screen w-screen flex justify-center'>
+      <div class="block w-8/12 h-8/12">
+        <Contador tiempoRestante={30}/>
+        <Reiniciar/>
+      </div>
     </div>
   )
 }
-
+// Componente que genera las palabras
+function GenerarPalabras(props){
+  return(
+      <p class="text-gray-700 text-5xl">{props.words}</p>
+  )
+}
+//Tiempo para jugar
+function Contador({tiempoRestante}){
+  return(
+    <h2 class="text-purple-800 text-5xl text-center">Tiempo: {tiempoRestante}</h2>
+  )
+}
 export default App
