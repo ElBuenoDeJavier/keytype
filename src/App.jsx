5624c49b5195e431d505785a2393455cb3c624b7
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-01 11:51:16 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className='text-center text-2xl font-bold bg-white dark:bg-black text-blue-100 place-content-center h-screen'>
       TEXTO DE PRUEBA PARA VERIFICAR QUE TODO ESTÁ FUNCIONANDO
+      
     </div>
   )
 }
