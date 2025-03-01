8075cb110ece890bfd7915f25750997ebb102107
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-01 11:28:55 +0100

    instalar dependencias
    He instalado el proyecto react con vite, tailwind con vite y configurado como dice la documentación para que el proyecto funcione

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div className='text-center text-2xl font-bold text-blue-500 place-content-center'>
+      TEXTO DE PRUEBA PARA VERIFICAR QUE TODO ESTÁ FUNCIONANDO
+    </div>
+  )
+}
+
+export default App
