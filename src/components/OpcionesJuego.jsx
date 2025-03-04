a861feee28d934c4cf18b43702f2e1b8dfd7ef97
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-04 12:11:42 +0100

    Mejoras botones

--- src/components/OpcionesJuego.jsx
+++ src/components/OpcionesJuego.jsx
@@ -1,22 +1,44 @@
 import React from "react";
-export default function OpcionesJuego(){
+export default function OpcionesJuego({contador}){
+    if(contador == false){
     return(
-        <div class="inline-flex rounded-md shadow-xs" role="group">
-        <button type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
-        border-indigo-800 rounded-s-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-indigo-400 focus:bg-indigo-800
-         focus:text-white">
-            10s
-        </button>
-        <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
-        border-indigo-800 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
-         focus:text-white">
-            20s
-        </button>
-        <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
-        border-indigo-800 rounded-e-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
-        focus:text-white">
-            30s
-        </button>
+        <div class="flex rounded-md shadow-xs justify-center mb-3" role="group">
+            <div className="mr-3">
+            <button type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
+            border-indigo-800 rounded-s-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-indigo-400 focus:bg-indigo-800
+            focus:text-white">
+                10s
+            </button>
+            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
+            border-indigo-800 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
+            focus:text-white">
+                20s
+            </button>
+            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
+            border-indigo-800 rounded-e-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
+            focus:text-white">
+                30s
+            </button>
+            </div>
+
+            <div className="ml-3">
+            <button type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
+            border-indigo-800 rounded-s-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-indigo-400 focus:bg-indigo-800
+            focus:text-white">
+                15 palabras
+            </button>
+            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
+            border-indigo-800 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
+            focus:text-white">
+                20 palabras
+            </button>
+            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
+            border-indigo-800 rounded-e-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
+            focus:text-white">
+                40 palabras
+            </button>
+            </div>
         </div>
     );
+    }
 } 
