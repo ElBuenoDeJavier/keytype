3056d6db5324d97b1a7cc5796e5cdb6931052ac0
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-05 12:37:02 +0100

    boton ajustes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,9 @@ function Header({contador}) {
                     <h1 className='text-5xl text-indigo-800'>⌨️KeyType</h1>
                 </div>
                 <div className="flex items-center gap-3">
-                    <button className="text-white px-5 py-2 rounded-full bg-indigo-800 hover:bg-indigo-600">Iniciar sesión</button>
+                    <button className="px-5 py-2"><svg class="h-8 w-8 text-indigo-800 hover:text-indigo-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />  <circle cx="12" cy="12" r="3" /></svg></button>
+                    <ion-icon name="menu" className="text-3xl cursor-pointer md:hidden"></ion-icon>
+                    <button className="text-white px-5 py-2 rounded-full bg-indigo-800 hover:bg-indigo-500">Iniciar sesión</button>
                     <ion-icon name="menu" className="text-3xl cursor-pointer md:hidden"></ion-icon>
                 </div>
             </nav>
