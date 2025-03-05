3fe6f5555a37420a7e1ce6e2e9de47aa486c8434
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-05 10:47:38 +0100

    cambios jeje

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({contador}) {
         <header className="bg-gray-950 font-mono absolute p-7 w-screen">
             <nav className="flex justify-between items-center mx-auto">
                 <div>
-                    <h1 className='text-5xl text-indigo-800'>⌨️KeyType</h1>
+                    <h1 className='text-5xl text-indigo-800'>⌨️Raquel💖💘</h1>
                 </div>
                 <div className="flex items-center gap-3">
                     <button className="text-white px-5 py-2 rounded-full bg-indigo-800 hover:bg-indigo-600">Iniciar sesión</button>
