e9c4eb0ba18c1efd2fdfae3b94d965518b6e8b15
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-05 12:18:55 +0100

    BotonesFuncionales

--- src/components/OpcionesJuego.jsx
+++ src/components/OpcionesJuego.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-export default function OpcionesJuego({contador}){
+export default function OpcionesJuego({contador, establecerTiempo, establecerPalabras}){
     if(contador == false){
     return(
         <div className="flex rounded-md shadow-xs justify-center mb-3" role="group">
             <div className="mr-3">
-            <button type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
+            <button onClick={establecerTiempo} value={10} type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
             border-indigo-800 rounded-s-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-indigo-400 focus:bg-indigo-800
             focus:text-white">
                 10s
             </button>
-            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
+            <button type="button" onClick={establecerTiempo} value={20} className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
             border-indigo-800 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
             focus:text-white">
                 20s
             </button>
-            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
+            <button type="button" onClick={establecerTiempo} value={30} className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
             border-indigo-800 rounded-e-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
             focus:text-white">
                 30s
@@ -22,17 +22,17 @@ export default function OpcionesJuego({contador}){
             </div>
 
             <div className="ml-3">
-            <button type="button" className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
+            <button type="button" onClick={establecerPalabras} value={15} className="px-4 py-2 text-2xl text-indigo-800 bg-transparent border 
             border-indigo-800 rounded-s-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-indigo-400 focus:bg-indigo-800
             focus:text-white">
                 15 palabras
             </button>
-            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
+            <button type="button" onClick={establecerPalabras} value={20} className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border-t border-b 
             border-indigo-800 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
             focus:text-white">
                 20 palabras
             </button>
-            <button type="button" className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
+            <button type="button" onClick={establecerPalabras} value={40} className="px-4 py-2 text-2xl font-medium text-indigo-800 bg-transparent border 
             border-indigo-800 rounded-e-lg hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-indigo-800
             focus:text-white">
                 40 palabras
