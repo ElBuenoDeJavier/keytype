56cdeb3def8619ffc9732e0e3c2b5f5b6a91fff5
Javier Uberos <156229400+ElBuenoDeJavier@users.noreply.github.com>
2025-03-11 00:34:47 +0100

    Mejoras Login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import mostrarLogin from './MostrarLogin';
 
 function Header({contador, pulsarBotonLogin, pulsarLogo, dataUsuario}) {
     if(contador == false){
@@ -11,10 +10,6 @@ function Header({contador, pulsarBotonLogin, pulsarLogo, dataUsuario}) {
                 </div>
                 <div className="flex items-center gap-3">
 
-                    {dataUsuario?<div  className='text-white flex'>Hola, {dataUsuario.name}<svg className="h-8 w-8 text-indigo-800 hover:text-indigo-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-                    </svg></div>:''}
-
                     <button className="px-5 py-2"><svg className="h-8 w-8 text-indigo-800 hover:text-indigo-500"  width="24" height="24" 
                     viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />  <circle cx="12" cy="12" r="3" /></svg></button>
                     
@@ -24,7 +19,11 @@ function Header({contador, pulsarBotonLogin, pulsarLogo, dataUsuario}) {
                     </svg>
                     </button>
                     
-                    <button onClick={pulsarBotonLogin} className="text-white px-5 py-2 rounded-full bg-indigo-800 hover:bg-indigo-500">Iniciar sesión</button>
+                    {!dataUsuario?<button onClick={pulsarBotonLogin} className="text-white px-5 py-2 rounded-full bg-indigo-800 hover:bg-indigo-500">Iniciar sesión</button>:''}
+
+                    {dataUsuario?<div  className='text-white flex'><svg className="h-8 w-8 text-indigo-800 hover:text-indigo-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                    </svg>{dataUsuario.name}</div>:''}
                 </div>
             </nav>
         </header>
